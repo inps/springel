812329ed9b90ba10abbb00b32ea2486f9289e722
inps <inps@gmail.com>
2019-08-04 12:12:57 +0800

    解决规则表达式中，参数的value与参数的key相同的问题。处理参数为空值的问题。

--- src/main/java/cn/inps/springel/rule/MyRule.java
+++ src/main/java/cn/inps/springel/rule/MyRule.java
@@ -1,33 +1,6 @@
 package cn.inps.springel.rule;
 
-import org.springframework.expression.AccessException;
-import org.springframework.expression.BeanResolver;
-import org.springframework.expression.EvaluationContext;
-
-import java.util.HashMap;
-import java.util.Map;
-
-
-//@Service("resolver")
-public class MyBeanResolver implements BeanResolver {
-
-    public Person resolve(EvaluationContext evaluationContext, String s) throws AccessException {
-        System.out.println("test:"+s);
-        System.out.println("te:"+evaluationContext.getRootObject().getValue().toString());
-
-        Map<String,Object> hm = new HashMap<String ,Object>();
-        hm =(Map<String, Object>) evaluationContext.getRootObject().getValue();
-        System.out.println("cc:"+hm.get("cc"));
-        System.out.println("bb:"+hm.get("bb"));
-
-
-        Person person = new Person();
-        person.setAge("age34");
-        person.setName("name34");
-        return person;
-    }
-
-
+public class MyRule {
 
     public static  Person  execute(){
         Person person = new Person();
@@ -53,11 +26,20 @@ public class MyBeanResolver implements BeanResolver {
     public static  Person  execute(String input,String input1,String input2){
         System.out.println("input:"+input);
         System.out.println("input1:"+input1);
+        System.out.println("input2:"+input2);
         Person person = new Person();
         person.setAge("age40");
         person.setName("name40");
         return person;
     }
-
-
+    public static  Person  execute(String input,String input1,String input2,String input3){
+        System.out.println("input:"+input);
+        System.out.println("input1:"+input1);
+        System.out.println("input2:"+input2);
+        System.out.println("input3:"+input3);
+        Person person = new Person();
+        person.setAge("age40");
+        person.setName("name40");
+        return person;
+    }
 }
--- src/main/java/cn/inps/springel/rule/SpringExpressionTest.java
+++ src/main/java/cn/inps/springel/rule/SpringExpressionTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.EvaluationException;
 import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.ParseException;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
@@ -12,6 +14,8 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpringExpressionTest {
     public  static Logger log  =  LoggerFactory.getLogger(SpringExpressionTest.class);
@@ -121,55 +125,135 @@ public class SpringExpressionTest {
         ExpressionParser parser = new SpelExpressionParser();
         StandardEvaluationContext ctx = new StandardEvaluationContext();
 
-        String className  = "cn.inps.springel.rule.MyBeanResolver";
+        //输入参数
+        String className  = "cn.inps.springel.rule.MyRule";
+        String  el  = "获取当(流程实例id,,'dd)";
+        int count = 0;
 
+
+        Map<String,Object>  lhm =  new LinkedHashMap<String,Object>();
+        lhm.put("当前人id","bbbb33");
+        lhm.put("流程定义id","cccc33");
+        lhm.put("流程实例id","dddd333");
+
+
+        //替换规则表达式中所有的空格和'符合
+        String expression=  el.replace(" ", "");
+        expression=  expression.replace("'", "");
+        expression=  expression.replaceFirst(el.substring(0,el.indexOf('(')),"#execute");
+
+        // 通过正则表达式获取小括号中的内容，表达式变为   #execute（'当前人id的值','流程定义id的值'）
+        Pattern pattern = Pattern.compile("\\([^)]+\\)");
+        Matcher matcher = pattern.matcher(expression);
+
+        if (matcher.find())
+        {
+            // System.out.println(matcher.group());
+            String quStr  = matcher.group(0);
+            quStr = matcher.group().substring(1, matcher.group().length()-1);
+
+            String[] ary = quStr.split(",");//使用字符串逗号 ,切割字符串
+            count = ary.length;
+            // 现将数据替换成#execute（{0},{1},{2}）, 如果出现了一个key的value 和后面要替换的key相同，可能会出现替换错误
+            for (int i = 0; i < count; i++) {
+                if(!String.valueOf(ary[i]).equals("")) {
+                    expression = expression.replace(String.valueOf(ary[i]), "{" + i + "}");
+                }
+            }
+            for (int i = 0; i < count; i++) {
+                expression = expression.replaceAll(String.format("\\{%d\\}", i), "'"+String.valueOf(lhm.get(ary[i]))+"'");
+            }
+            log.info("需要执行的表单式:{}",expression);
+
+//            for(String item: ary){
+//                //如果出现了一个key的value 和后面要替换的key相同，可能会出现替换错误
+//                String value = "'"+lhm.get(item)+"'";
+//                expression = expression.replace(item,value);
+//            }
+        }
+
+        //执行规则对应的方法， 没有在规则中使用 BeanResolver resolve， 由于规则编写过程中不知道map<string ,object>中的参数含义，对规则编写不友好。
         Method executeMethod = null;
         try {
 
             if(className!=null||!className.trim().equals("")){
             //  execute = MyBeanResolver.class.getDeclaredMethod("execute");
                 try {
-                    executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class,String.class);
-
-
-
+                    if(count==0){
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute");
+                    }else if(count==1) {
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class);
+                    }else if(count==2){
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class,String.class);
+                    }else if(count==3){
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class,String.class,String.class);
+                    }else if(count==4){
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class,String.class,String.class,String.class);
+                    }else if(count==5){
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class,String.class,String.class,String.class,String.class);
+                    }else if(count==6){
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class,String.class,String.class,String.class,String.class,String.class);
+                    }else if(count==7){
+                        executeMethod = Class.forName(className).getDeclaredMethod("execute", String.class,String.class,String.class,String.class,String.class,String.class,String.class);
+                    }else{
+                        log.info("规则方法参数数量不正确，规则参数大于7个！");
+                    };
                 } catch (ClassNotFoundException e) {
+                    log.info("没有找到规则对应的类");
                     e.printStackTrace();
                 }
             }
         } catch (NoSuchMethodException e) {
+            log.info("没有找到规则对应的方法");
             e.printStackTrace();
         }
+
+        //注册方法
         ctx.registerFunction("execute", executeMethod);
 
-        Map<String,Object>  lhm =  new LinkedHashMap<String,Object>();
-        lhm.put("当前人id","bbbb33");
-        lhm.put("流程定义id","cccc33");
-        lhm.put("流程实例id","dddd333");
-        ctx.setVariables(lhm);
+        //增加变量, 由于不支持中文变量， 直接将值传入表达式  #execute（'当前人id的值','流程定义id的值'），没有采用以下方式
+        //ctx.setVariables(lhm);
+        // String expression ="#execute(#currentid,#processdefid)";
+        // String expression = convertExpression(elstr,lhm);
 
-
-        String  elstr  = "获取当前处理人(当前人id,流程定义id)";
-        String expression = getEL(elstr,lhm);
-
-       // String expression ="#execute(#currentid,#processdefid)";
 
 
         //执行方法1
        // Person resultp  = parser.parseExpression("#execute('dddd','xxx')").getValue(ctx,Person.class);
-       Person resultp  = parser.parseExpression(expression).getValue(ctx,Person.class);
+        Person resultp  = null;
+        try {
+            resultp = parser.parseExpression(expression).getValue(ctx, Person.class);
+        } catch (EvaluationException e) {
+            log.info("表达式格式异常");
+            e.printStackTrace();
+        } catch (ParseException e) {
+            log.info("解析表达式异常");
+            e.printStackTrace();
+        } catch (Exception e) {
+            log.info("解析表达式格式异常，参数不能为空值。");
+            e.printStackTrace();
+        }
         log.info("executeperson result:{}",resultp.getName());
 
     }
-    public String  getEL(String el,Map<String,Object> hm){
+
+
+    public String  convertExpression(String el,Map<String,Object> hm){
 
          // el  = "获取当前处理人(当前人id,流程定义id)";
         String expression=  el.replace(" ", "");
         expression=  el.replaceFirst(el.substring(0,el.indexOf('(')),"#execute");
-        String quStr=expression.substring(el.indexOf("(")+2,el.indexOf(")")+1);
-        //String quStr = expression.replaceAll("\\(.*?\\)|\\{.*?}|\\[.*?]|（.*?）", "");
 
-        if(quStr!=null|| !"".equals(quStr)){
+        // 通过正则表达式获取小括号中的内容
+        Pattern pattern = Pattern.compile("\\([^)]+\\)");
+        Matcher matcher = pattern.matcher(expression);
+
+        if (matcher.find())
+        {
+           // System.out.println(matcher.group());
+            String quStr  = matcher.group(0);
+            quStr = matcher.group().substring(1, matcher.group().length()-1);
+
             String[] ary = quStr.split(",");//使用字符串逗号 ,切割字符串
             for(String item: ary){
                 String value = "'"+hm.get(item)+"'";
@@ -177,10 +261,26 @@ public class SpringExpressionTest {
             }
         }
         System.out.println(expression);
-
         //String expression ="#execute(#currentid,#processdefid)";
         return expression;
     }
+    public int getParametesCount(String elstr){
+        int count =0;
+        String expression=  elstr.replace(" ", "");
+        // 通过正则表达式获取小括号中的内容
+        Pattern pattern = Pattern.compile("\\([^)]+\\)");
+        Matcher matcher = pattern.matcher(expression);
 
+        if (matcher.find())
+        {
+            // System.out.println(matcher.group());
+            String quStr  = matcher.group(0);
+            quStr = matcher.group().substring(1, matcher.group().length()-1);
+            String[] ary = quStr.split(",");//使用字符串逗号 ,切割字符串
+            count = ary.length;
+        }
+
+        return count;
+    }
 
 }
